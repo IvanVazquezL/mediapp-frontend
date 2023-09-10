8b2c99ca5882c2c448c81c77bb5719265048cb20
IvanVazquezL <ivanvl_11@hotmail.com>
2023-09-09 18:54:10 -0600

    Login and Patient Components added and their routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { PatientComponent } from './pages/patient/patient.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'pages/patient', component: PatientComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
